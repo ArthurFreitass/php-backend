d01c30cd0df2e9ed8311aa96e5eb4ef4b761f45e
Arthur Freitas <freitarthur176@gmail.com>
2026-09-21 11:07:06 -0300

    Add files via upload

---
+++ supermarket/php/register_supplier.php
@@ -0,0 +1,12 @@
+<?php 
+
+include_once('connection.php');
+
+$name = $_POST['name'];
+$tax_id = $_POST['tax_id'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$status = $_POST['status'];
+
+$insertBD = mysqli_query($connection, )
+?>
